a46f656e573dc9f52214cff72f8a6925329237d3
HongMuchan <muchang0824@gmail.com>
2020-09-29 09:14:44 +0900

    [Add]increment and decrement add actions

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const INREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+//アクションクリエーターの定義
+export const increment = () => {
+    return {
+        type: "INCREMENT"
+    }
+}
+export const decrement = () => {
+    return {
+        type: "DECREMENT"
+    }
+}
